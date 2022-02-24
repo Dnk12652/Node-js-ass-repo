db65b6e799da41174b7b4147dbb7d60ab8902d64
Dnk12652 <95068562+Dnk12652@users.noreply.github.com>
2022-02-24 12:17:52 +0530

    Update index.js

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -4,13 +4,11 @@ function getNameFromCommandLine() {
     return process.argv[argv.length-1]
     // Write you code here, name should be taken as args in process.argv
 }
-
 function getNameFromEnv() {
     return process.env.name
-    
+
     // Write your code here
 }
-
 function getNameFromReadLine() {
     var readline=require('readline')
     readline.createInterface({
@@ -22,7 +20,7 @@ function getNameFromReadLine() {
     })
 }
     // Write your code here
-}
+
 
 module.exports = {
     getNameFromCommandLine,
